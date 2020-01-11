28754e0767c9485d90afe89b5066e279508e9dbd
rauzola <raul_sigoli@hotmail.com>
2020-01-11 15:55:11 -0300

    Usando o Flutter para consulta um api na net e devolver o precodo biticoin

---
+++ preco_biticoin/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:preco_biticoin/Home.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  )
+);
